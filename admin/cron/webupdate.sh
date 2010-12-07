3474908b30394739cdab755a1c15b4a997eef273
Akihiro MOTOKI <amotoki@users.sourceforge.jp>
2010-12-08 03:44:03 +0900

    Add a shell to update web contents periodically.

---
+++ admin/cron/webupdate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export LANG=C
+export LC_TIME=C
+
+JMHOME=/home/groups/l/li/linuxjm
+GITDIR=$JMHOME/jm.git
+LOGDIR=$JMHOME/log
+
+test -d $LOGDIR || mkdir -p $LOGDIR
+LOGFILE=$LOGDIR/log-$(/bin/date +%Y%m%d-%H%M%S).txt
+touch $LOGFILE
+
+echo "===== git pull =====" >> $LOGFILE 2>&1
+cd $JMHOME/jm.git
+git pull >> $LOGFILE 2>&1
+
+echo "===== Update web contents =====" >> $LOGFILE 2>&1
+make >> $LOGFILE 2>&1
+
+# clean up logs
+find $LOGDIR -mtime +30 -exec rm -f {} \; -print >> $LOGFILE 2>&1
