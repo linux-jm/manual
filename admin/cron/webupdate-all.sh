aa029dd63b6d43db4f973c6f0e3e04d36dc47520
Akihiro MOTOKI <amotoki@users.sourceforge.jp>
2010-12-09 01:50:12 +0900

    Add a another shell to update web and manpage archives.

---
+++ admin/cron/webupdate-all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LANG=C
+export LC_TIME=C
+
+JMHOME=/home/groups/l/li/linuxjm
+GITDIR=$JMHOME/jm.git
+LOGDIR=$JMHOME/log
+
+test -d $LOGDIR || mkdir -p $LOGDIR
+LOGFILE=$LOGDIR/log-$(/bin/date +%Y%m%d-%H%M%S).txt
+touch $LOGFILE
+
+cd $JMHOME/jm.git
+echo "===== git pull =====" >> $LOGFILE 2>&1
+git pull >> $LOGFILE 2>&1
+
+echo "===== Update web contents =====" >> $LOGFILE 2>&1
+rm admin/site/sitemap.xml
+make all >> $LOGFILE 2>&1
+
+# clean up logs
+find $LOGDIR -mtime +30 -exec rm -f {} \; -print >> $LOGFILE 2>&1
