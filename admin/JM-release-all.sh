e074af48e5b40bab1c142cc856576b76a21ba265
Akihiro MOTOKI <amotoki@gmail.com>
2013-08-16 16:09:07 +0900

    admin (JM-release.sh): Skip if draft and release pages are same
    Add the following options:
    * -c option indicates JM-release.sh just copies draft to release
      instead of using sed to remove JM comments.
    * -v option specifies the verbose mode.
      Skipped files are displayed in the verbose mode.

--- admin/JM-release-all.sh
+++ admin/JM-release-all.sh
@@ -3,6 +3,19 @@
 CMDDIR=$(dirname $0)
 CMD=$CMDDIR/JM-release.sh
 
-for f in `find draft/ -type f`; do
-  $CMD -f -g $f release
+OPTS="-f -g"
+
+# optparse
+while getopts "cv" OPT
+do
+  case $OPT in
+    "c") OPTS="${OPTS} -c" ;;
+    "v") OPTS="${OPTS} -v" ;;
+  esac
+done
+
+for d in draft/man?; do
+  for f in `find $d -type f | sort`; do
+    $CMD $OPTS $f release
+  done
 done
--- admin/JM-release.sh
+++ admin/JM-release.sh
@@ -7,18 +7,24 @@ SED=/bin/sed
 usage() {
     echo "Usage: `basename $0` <draft file> <release directory>"
     echo "Options:"
+    echo "  -c : Copy mode. By default, sed is used to remove JM comments."
     echo "  -f : Force Override the existing page"
     echo "  -g : Guess the release directory from the draft location"
+    echo "  -v : Verbose mode"
 }
 
+COPY=0
 FORCE=0
 GUESS=0
+VERBOSE=0
 
-while getopts fg OPT
+while getopts "cfgv" OPT
 do
   case $OPT in
+    "c") COPY=1 ;;
     "f") FORCE=1 ;;
     "g") GUESS=1 ;;
+    "v") VERBOSE=1 ;;
     *)   usage ;;
   esac
 done
@@ -55,6 +61,13 @@ if [ ! -d $RELDIR ]; then
   exit 1
 fi
 
+if test -f $RELEASE && cmp $DRAFT $RELEASE > /dev/null; then
+  if [ $VERBOSE -eq 1 ]; then
+    echo "Skipped $(basename $DRAFT): draft and release pages are same."
+  fi
+  exit 0
+fi
+
 if [ -f $RELEASE -a $FORCE -ne 1 ]; then
   echo -n "Overwrite $RELEASE (y/N) "
   read ret
@@ -67,7 +80,11 @@ if [ -f $RELEASE -a $FORCE -ne 1 ]; then
   fi
 fi
 
-$SED -e '/^\.\\\"O/d' $DRAFT > $RELEASE
-echo "$DRAFT -> $RELEASE"
+if [ $COPY -eq 1 ]; then
+  cp -fpv $DRAFT $RELEASE
+else
+  $SED -e '/^\.\\\"O /d' $DRAFT > $RELEASE
+  echo "$DRAFT -> $RELEASE"
+fi
 
 #echo "Succeed"
