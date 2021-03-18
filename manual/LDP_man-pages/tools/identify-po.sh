be245011954572da591314f6bcb0764f948d157f
Akihiro Motoki <amotoki@gmail.com>
2021-03-18 13:03:54 +0900

    LDP: Fix identify-po.sh
    * Catch up perkamon merge into the main directory
    * Handle manpage name more explicitly.
      Previously "identify-po.sh open.2" did not work expectedly
      and a wrong category was picked up.

--- manual/LDP_man-pages/tools/identify-po.sh
+++ manual/LDP_man-pages/tools/identify-po.sh
@@ -22,11 +22,11 @@ if [ $(basename $DIR) != "LDP_man-pages" ]; then
     exit 1
 fi
 
-CFG=$(grep -l $PAGENAME perkamon/po4a/*/*.cfg)
+CFG=$(grep -l /$PAGENAME po4a/*/*.cfg)
 if [ -z "$CFG" ]; then
     echo "[ERROR] PO file for $PAGENAME is not found!"
     exit 2
 fi
-SEC=$(echo $CFG | cut -d / -f 3)
+SEC=$(echo $CFG | cut -d / -f 2)
 POFILE=po4a/$SEC/po/ja.po
 echo "$PAGENAME -> $POFILE"
